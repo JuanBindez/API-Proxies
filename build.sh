303db95ab2302bc72e6b3bcfbb75a834057d0d41
Juan Bindez <juanbindez780@gmail.com>
2026-09-16 23:42:04 -0300

    0.0.1 Init: Projeto iniciado Proxies API
    Signed-off-by: Juan Bindez <juanbindez780@gmail.com>

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+VERSION=0
+MINOR=0
+PATCH=1
+EXTRAVERSION=""
+COMMIT="Init: Projeto iniciado Proxies API
+
+
+
+"
+BRANCH="main"
+
+if [[ -z $PATCH ]]; then
+    PATCH=""
+else
+    PATCH=".$PATCH"
+fi
+
+if [[ $EXTRAVERSION == *"-rc"* ]]; then
+    FULL_VERSION="$VERSION.$MINOR$PATCH$EXTRAVERSION"
+else
+
+    if [[ -z $EXTRAVERSION ]]; then
+        FULL_VERSION="$VERSION.$MINOR$PATCH"
+    else
+        FULL_VERSION="$VERSION.$MINOR$PATCH.$EXTRAVERSION"
+    fi
+fi
+
+git add .
+git commit -s -m "$FULL_VERSION $COMMIT"
+git push -u origin $BRANCH
+git tag v$FULL_VERSION
+git push --tags
+
+echo "Build $FULL_VERSION completed successfully!"
